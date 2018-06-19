a0f7a16045c186262f6fd5186e91421a18cee658
HASLima <39990914+HASLima@users.noreply.github.com>
2018-06-19 01:39:34 +0100

    Game Operations added
    Added gameOperations.c and gameOperations.h
    These files are supposed to manage all the games operations.
    Implemented global methods DaysGone and Money to return the int DaysGone
    and the double Money.

---
+++ smartFishing/gameOperations.c
@@ -0,0 +1,23 @@
+/*
+ * gameOperations.c
+ *
+ *  Created on: 19/06/2018
+ *      Author: haslima
+ */
+#include <stdio.h>
+#include "gameOperations.h"
+
+int daysGone; /* number of game days the user have played */
+double money; /* the amount of money the user has*/
+
+int DaysGone()
+{
+	return daysGone;
+}
+
+double Money()
+{
+	return money;
+}
+
+
---
+++ smartFishing/gameOperations.h
@@ -0,0 +1,18 @@
+/*
+ * gameOperations.h
+ *
+ *  Created on: 19/06/2018
+ *      Author: haslima
+ */
+
+#ifndef GAMEOPERATIONS_H_
+#define GAMEOPERATIONS_H_
+
+
+extern int DaysGone(void);
+
+extern double Money(void);
+
+
+
+#endif /* GAMEOPERATIONS_H_ */
