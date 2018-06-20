58f939c0a1249610e0a19333d6d4c77b73c4ebe8
HASLima <39990914+HASLima@users.noreply.github.com>
2018-06-20 02:43:00 +0100

    Added DayPassed() Method
    Added DayPassed() Method which can be used to increment the daysGone
    int.
    Commented gameOperations.c Methods
    Added DayPassed() Metod to the .h file.

--- smartFishing/gameOperations.c
+++ smartFishing/gameOperations.c
@@ -10,12 +10,17 @@
 int daysGone = 0; /* number of game days the user have played */
 double money = 0; /* the amount of money the user has*/
 
-int DaysGone()
+int DaysGone() /* Returns the number of game days since the beginning of the game*/
 {
 	return daysGone;
 }
 
-double Money()
+void DayPassed() /*Adds one day to daysGone*/
+{
+	daysGone++;
+}
+
+double Money() /*Returns the money in bank account*/
 {
 	return money;
 }
